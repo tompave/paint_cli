6e4dfeed4f74a12da4426b70d69116f7856ccf96
tom <tommaso@pavese.me>
2015-06-08 00:48:59 +0100

    implemented basic functionality of new image operation

--- lib/paint_cli/operation/new_image.rb
+++ lib/paint_cli/operation/new_image.rb
@@ -1,4 +1,14 @@
 module PaintCli::Operation
   class NewImage < Base
+
+    def initialize(args_array=nil)
+      @args = args_array
+      @w, @h = args
+    end
+
+
+    def process(input_bmp=nil)
+      PaintCli::Bitmap.new(@w, @h)
+    end
   end
 end
--- test/operation/new_image_test.rb
+++ test/operation/new_image_test.rb
@@ -4,6 +4,28 @@ class NewImageTest < Minitest::Test
 
   def setup
     super
+    @args = [7, 5]
+    @operation = PaintCli::Operation::NewImage.new(@args)
+  end
+
+
+  def test_process_returns_a_bitmap
+    assert_instance_of PaintCli::Bitmap, @operation.process
+    assert_instance_of PaintCli::Bitmap, @operation.process(nil)
+    assert_instance_of PaintCli::Bitmap, @operation.process(PaintCli::Bitmap.new(1,1))
+  end
+
+
+  def test_new_bitmap_has_right_size
+    bmp = @operation.process
+    assert_equal 7, bmp.width
+    assert_equal 5, bmp.height
+  end
+
+
+  def test_new_bitmap_is_blank
+    expected = PaintCli::Bitmap.new(7, 5)
+    assert_equal expected.render, @operation.process.render
   end
 
 end
