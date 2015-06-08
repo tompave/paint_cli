0c6cfaa5c1b8fc8b1fe9ebd0c5b128ad941b2b00
tom <tommaso@pavese.me>
2015-06-08 18:02:30 +0100

    implemented VerticalLine operation

--- lib/paint_cli/operation/vertical_line.rb
+++ lib/paint_cli/operation/vertical_line.rb
@@ -1,4 +1,27 @@
 module PaintCli::Operation
   class VerticalLine < Base
+
+    def initialize(args_array)
+      @args = args_array
+      @x, @y1, @y2, @color = line_coords
+    end
+
+
+    def process(input)
+      bmp = input
+
+      (@y1..@y2).each do |y|
+        bmp.change_pixel(@x, y, @color)
+      end
+
+      bmp
+    end
+
+    private
+
+    def line_coords
+      args[0..3]
+    end
+
   end
 end
--- test/operation/vertical_line_test.rb
+++ test/operation/vertical_line_test.rb
@@ -4,6 +4,42 @@ class VerticalLineTest < Minitest::Test
 
   def setup
     super
+    @bmp = PaintCli::Bitmap.new(6, 11)
+    @args = [3, 2, 8, "C"]
+    @operation = PaintCli::Operation::VerticalLine.new(@args)
+  end
+
+
+  def test_process_returns_a_different_bitmap
+    old = @bmp.render
+    out = @operation.process(@bmp)
+    refute_equal old, out.render
+  end
+
+
+  def test_process_returns_a_bitmap_of_the_same_size
+    out = @operation.process(@bmp)
+    assert_equal @bmp.width, out.width
+    assert_equal @bmp.height, out.height
+  end
+
+
+  def test_output_has_been_colored
+    expected = <<-EOS
+OOOOOO
+OOCOOO
+OOCOOO
+OOCOOO
+OOCOOO
+OOCOOO
+OOCOOO
+OOCOOO
+OOOOOO
+OOOOOO
+OOOOOO
+    EOS
+
+    assert_equal expected.strip, @operation.process(@bmp).render
   end
 
 end
