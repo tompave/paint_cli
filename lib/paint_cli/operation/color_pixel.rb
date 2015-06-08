22fcd50f926d0ed6a4a2c55381e46cb4b3db58b7
tom <tommaso@pavese.me>
2015-06-08 10:00:51 +0100

    implemented ColorPixel operation

--- lib/paint_cli/operation/color_pixel.rb
+++ lib/paint_cli/operation/color_pixel.rb
@@ -1,4 +1,15 @@
 module PaintCli::Operation
   class ColorPixel < Base
+    def initialize(args_array=nil)
+      @args = args_array
+      @x, @y, @color = args
+    end
+
+
+    def process(input_bmp)
+      bmp = input_bmp
+      bmp.change_pixel(@x, @y, @color)
+      bmp
+    end
   end
 end
--- test/operation/color_pixel_test.rb
+++ test/operation/color_pixel_test.rb
@@ -7,18 +7,20 @@ class ColorPixelTest < Minitest::Test
     @bmp = PaintCli::Bitmap.new(5, 4)
     @args = [2, 3, "C"]
     @operation = PaintCli::Operation::ColorPixel.new(@args)
-    @out = @operation.process(@bmp)
   end
 
 
   def test_process_returns_a_different_bitmap
-    refute_equal @bmp.render, @out.render
+    old = @bmp.render
+    out = @operation.process(@bmp)
+    refute_equal old, out.render
   end
 
 
   def test_process_returns_a_bitmap_of_the_same_size
-    assert_equal @bmp.width, @out.width
-    assert_equal @bmp.height, @out.height
+    out = @operation.process(@bmp)
+    assert_equal @bmp.width, out.width
+    assert_equal @bmp.height, out.height
   end
 
 
@@ -30,6 +32,6 @@ OCOOO
 OOOOO
     EOS
 
-    assert_equal expected, @out.render
+    assert_equal expected.strip, @operation.process(@bmp).render
   end
 end
