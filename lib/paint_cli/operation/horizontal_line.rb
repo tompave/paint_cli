cb8e7abb3a088e00c2bf2e12c695b7d098ed4150
tom <tommaso@pavese.me>
2015-06-08 17:58:43 +0100

    implemented HorizontalLine operation

--- lib/paint_cli/operation/horizontal_line.rb
+++ lib/paint_cli/operation/horizontal_line.rb
@@ -1,4 +1,26 @@
 module PaintCli::Operation
   class HorizontalLine < Base
+
+    def initialize(args_array)
+      @args = args_array
+      @x1, @x2, @y, @color = line_coords
+    end
+
+
+    def process(input)
+      bmp = input
+
+      (@x1..@x2).each do |x|
+        bmp.change_pixel(x, @y, @color)
+      end
+
+      bmp
+    end
+
+    private
+
+    def line_coords
+      args[0..3]
+    end
   end
 end
--- test/operation/horizontal_line_test.rb
+++ test/operation/horizontal_line_test.rb
@@ -4,6 +4,37 @@ class HorizontalLineTest < Minitest::Test
 
   def setup
     super
+    @bmp = PaintCli::Bitmap.new(11, 6)
+    @args = [3, 7, 2, "C"]
+    @operation = PaintCli::Operation::HorizontalLine.new(@args)
+  end
+
+
+  def test_process_returns_a_different_bitmap
+    old = @bmp.render
+    out = @operation.process(@bmp)
+    refute_equal old, out.render
+  end
+
+
+  def test_process_returns_a_bitmap_of_the_same_size
+    out = @operation.process(@bmp)
+    assert_equal @bmp.width, out.width
+    assert_equal @bmp.height, out.height
+  end
+
+
+  def test_output_has_been_colored
+    expected = <<-EOS
+OOOOOOOOOOO
+OOCCCCCOOOO
+OOOOOOOOOOO
+OOOOOOOOOOO
+OOOOOOOOOOO
+OOOOOOOOOOO
+    EOS
+
+    assert_equal expected.strip, @operation.process(@bmp).render
   end
 
 end
