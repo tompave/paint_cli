a81004d81454120d41a91e0c937aa1e381e192ab
tom <tommaso@pavese.me>
2015-06-08 09:14:32 +0100

    tests for ColorPixel operation

--- test/operation/color_pixel_test.rb
+++ test/operation/color_pixel_test.rb
@@ -4,5 +4,32 @@ class ColorPixelTest < Minitest::Test
 
   def setup
     super
+    @bmp = PaintCli::Bitmap.new(5, 4)
+    @args = [2, 3, "C"]
+    @operation = PaintCli::Operation::ColorPixel.new(@args)
+    @out = @operation.process(@bmp)
+  end
+
+
+  def test_process_returns_a_different_bitmap
+    refute_equal @bmp.render, @out.render
+  end
+
+
+  def test_process_returns_a_bitmap_of_the_same_size
+    assert_equal @bmp.width, @out.width
+    assert_equal @bmp.height, @out.height
+  end
+
+
+  def test_output_has_been_colored
+    expected = <<-EOS
+OOOOO
+OOOOO
+OCOOO
+OOOOO
+    EOS
+
+    assert_equal expected, @out.render
   end
 end
